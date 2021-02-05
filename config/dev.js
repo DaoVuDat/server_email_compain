04ee460284bdb6b4877bd74f6560ebbcd77bf31b
Dao Vu Dat <dat.daovu@gmail.com>
2021-02-05 16:31:09 +0700

    finish auth flow

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js
+module.exports = {
+    googleClientId: "222589176642-0toqgh2op62oj3igasu280gf9mmtpd8g.apps.googleusercontent.com",
+    googleClientSecret: "ddZFbqVbpimUXBNGo2j_dJVM",
+    mongoURI: "mongodb+srv://daovudat:khongnho@emaily.6bmau.mongodb.net/emailydb?retryWrites=true&w=majority",
+    cookieKey: "redacted",
+};
--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+// Create Models - Create Schema first. Then we could use mongoose.model("name") to create an instance
+require("./models/User");
+
+// To execute the passport.js code
+require("./services/passport");
+
+const keys = require("./config/keys");
+
+// CONFIGURATION
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send({ hi: "there"});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// add route
+require("./routes/authRoutes")(app);
+
 
 const PORT = process.env.PORT || 5000;
 
